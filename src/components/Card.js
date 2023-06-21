9a8bf413003808a38f430777bf9297515ee75037
joan lovelle alfeche <jlovellealfeche@gmail.com>
2023-06-21 15:58:36 +0200

    update card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,9 +4,7 @@ import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
 
 const Card = ({ title, description, imageSrc }) => {
-  const greeting = "Hello!";
-  const bio1 = "I am a Front end Developer";
-  const bio2 = "Passionate about building web sites and web applications.";
+  
 
   return (
     <VStack spacing={4} alignItems="center">
@@ -15,18 +13,7 @@ const Card = ({ title, description, imageSrc }) => {
         {title}
       </Heading>
       <Text>{description}</Text>
-      <HStack spacing={2}>
-        <Text>Greeting:</Text>
-        <Text>{greeting}</Text>
-      </HStack>
-      <HStack spacing={2}>
-        <Text>Bio 1:</Text>
-        <Text>{bio1}</Text>
-      </HStack>
-      <HStack spacing={2}>
-        <Text>Bio 2:</Text>
-        <Text>{bio2}</Text>
-      </HStack>
+      
       <FontAwesomeIcon icon={faArrowRight} />
     </VStack>
   );
--- src/components/LandingSection.js
+++ src/components/LandingSection.js
@@ -2,12 +2,10 @@ import React from "react";
 import { Avatar, Heading, VStack } from "@chakra-ui/react";
 import FullScreenSection from "./FullScreenSection";
 
-const greeting = "Hello, I am Pete!";
+const greeting = "Hello, I am Love!";
 const bio1 = "A frontend developer";
-const bio2 = "specialised in React";
+const bio2 = "Passionate in learning and applying skills i learned about building web sites and web applications.";
 
-// Implement the UI for the LandingSection component according to the instructions.
-// Use a combination of Avatar, Heading and VStack components.
 const LandingSection = () => (
   <FullScreenSection
     justifyContent="center"
@@ -15,7 +13,18 @@ const LandingSection = () => (
     isDarkBackground
     backgroundColor="#2A4365"
   >
-
+    <VStack spacing={4} alignItems="center">
+      <Avatar size="2xl" name="love" src="https://example.com/avatar.jpg" />
+      <Heading as="h1" size="2xl" color="white">
+        {greeting}
+      </Heading>
+      <Heading as="h2" size="lg" color="white">
+        {bio1}
+      </Heading>
+      <Heading as="h2" size="m" color="white">
+        {bio2}
+      </Heading>
+    </VStack>
   </FullScreenSection>
 );
 
